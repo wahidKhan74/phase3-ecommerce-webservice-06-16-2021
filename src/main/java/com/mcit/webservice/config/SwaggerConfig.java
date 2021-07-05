2b38dd8bf37843443d2ca0422860c66157bdd3fe
Waheed Khan <wahid.khan74@gmail.com>
2021-07-05 10:48:51 +0000

    adding cors servlet filter

--- src/main/java/com/mcit/webservice/config/SwaggerConfig.java
+++ src/main/java/com/mcit/webservice/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.mcit.webservice;
+package com.mcit.webservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
